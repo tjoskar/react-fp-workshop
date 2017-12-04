f5efa0bb284dc7c5c22697403b112fb5fef7a16a
tjoskar <kontakta@oskarkarlsson.nu>
2017-12-04 22:17:31 +0100

    🔥 Remove solution for 1

--- src/store/search/search.epic.ts
+++ src/store/search/search.epic.ts
@@ -1,6 +1,6 @@
 import { Observable } from 'rxjs/Observable'
 import { ajax } from 'rxjs/observable/dom/ajax'
-import { map, debounceTime, filter, switchMap } from 'rxjs/operators'
+import { map, filter, tap } from 'rxjs/operators'
 import { path } from 'ramda'
 import { UPDATE_SEARCH_FIELD, updateSeachResult } from './search.actions'
 
@@ -9,14 +9,26 @@ type UpdateSearchFieldAction = {
   value: string
 }
 
+type TvMazeShow = {
+  show: {
+    id: number
+    name: string
+    image?: {
+      medium?: string
+    }
+    summary: string
+  }
+}
+
 export const searchEpic = (action$: Observable<UpdateSearchFieldAction>) =>
   action$.pipe(
     filter(isUpdateSearchFieldAction),
-    map(action => action.value),
-    filter(isSearchTermLongerThan2),
-    debounceTime(1000),
-    switchMap(searchForShow),
-    map(mapShows),
+    tap(action => console.log(action)),
+    // filter out short search strings
+    // wait for 500 ms
+    // make the ajax request (searchForShow)
+    // call `updateSeachResult` with the result from mapShows
+    map(() => []),
     map(updateSeachResult)
   )
 
@@ -29,7 +41,7 @@ export const emptySearchResultEpic = (action$: Observable<UpdateSearchFieldActio
     map(updateSeachResult)
   )
 
-const searchForShow = (seatchTerm: string) =>
+export const searchForShow = (seatchTerm: string): Observable<TvMazeShow[]> =>
   ajax({
     url: `http://api.tvmaze.com/search/shows?q=${seatchTerm}`,
     method: 'GET',
@@ -46,11 +58,11 @@ function isUpdateSearchFieldAction(action: UpdateSearchFieldAction) {
   return action.type === UPDATE_SEARCH_FIELD
 }
 
-function mapShows(tvMazeShows) {
+export function mapShows(tvMazeShows: TvMazeShow[]) {
   return tvMazeShows.map(show => mapShow(show.show))
 }
 
-function mapShow(tvMazeShow) {
+function mapShow(tvMazeShow: TvMazeShow['show']) {
   return {
     id: tvMazeShow.id,
     title: tvMazeShow.name,
