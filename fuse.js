5a56c05c9954a8a6a4a2c0685a45f17f81a48ba1
tjoskar <kontakta@oskarkarlsson.nu>
2017-12-04 16:21:53 +0100

    🐛  Add history-api-fallback for the dev server

--- fuse.js
+++ fuse.js
@@ -1,8 +1,13 @@
+const path = require('path')
+const express = require('express')
+const fallback = require('express-history-api-fallback')
 const { FuseBox } = require('fuse-box')
+
 const fuse = FuseBox.init({
   homeDir: 'src',
   output: 'dist/$name.js'
 })
+
 fuse
   .bundle('app')
   .sourceMaps(true)
@@ -10,9 +15,16 @@ fuse
   .instructions(`>boot.ts`)
   .watch()
 
-fuse.dev({
-  port: 8080,
-  root: '.'
-})
+fuse.dev(
+  {
+    port: 8080,
+    root: '.'
+  },
+  server => {
+    const root = path.resolve('.')
+    server.httpServer.app.use(express.static(root))
+    server.httpServer.app.use(fallback('index.html', { root }))
+  }
+)
 
 fuse.run()
